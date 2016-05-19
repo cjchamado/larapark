9a19c19c871ef13b04217057261d609bc4310000
Claudinei Machado <cjchamado@gmail.com>
2016-05-19 20:37:49 -0300

    Adicionado: database

---
+++ database/migrations/2016_05_19_231708_create_veiculos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVeiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('veiculos', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('placa', 15);
+            $table->string('marca', 15);
+            $table->string('modelo', 15);
+            $table->string('cor', 15);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('veiculos');
+    }
+}
