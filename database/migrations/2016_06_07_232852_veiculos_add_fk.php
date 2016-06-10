c52c412ce6e6e1ddc2707eae29aec13900de2b78
Claudinei Machado <cjchamado@gmail.com>
2016-06-09 22:30:25 -0300

    Add migration for foreign key

---
+++ database/migrations/2016_06_07_232852_veiculos_add_fk.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class VeiculosAddFk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('veiculos', function (Blueprint $table) {
+            //$table->integer('cliente_id')->unsigned();
+
+            $table->foreign('cliente_id')
+                    ->references('id')
+                    ->on('clientes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('veiculos', function (Blueprint $table) {
+            //
+        });
+    }
+}
